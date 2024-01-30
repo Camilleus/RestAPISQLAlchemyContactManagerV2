e430f96b8eeeb51648574ae7a3fd6cf487784472
CamilleusRex <camilleusrex@gmail.com>
2024-01-30 21:27:04 +0100

    added login acces and current active user

--- auth.py
+++ auth.py
@@ -1,13 +1,18 @@
-from fastapi import HTTPException, Depends, status
+from fastapi import HTTPException, Depends
 from datetime import datetime, timedelta
+from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
-from auth import oauth2_scheme
-from models import Token, TokenData, User
+from passlib.context import CryptContext
+from models import Token, User
+from fastapi import status
 
 
 SECRET_KEY = "your_secret_key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 def authenticate_user(username: str, password: str):
@@ -24,6 +29,20 @@ def create_access_token(data: dict, expires_delta: timedelta):
     return encoded_jwt
 
 
+def login_for_access_token(form_data: OAuth2PasswordBearer = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
 def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -35,6 +54,12 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-        token_data = TokenData(username=username)
+        token_data = Token(username=username)
     except JWTError:
         raise credentials_exception
+    
+    
+def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user is None:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
