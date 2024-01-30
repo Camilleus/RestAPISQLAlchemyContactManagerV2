90a64cd8ee1bd97c9c400f22b5f95714300c7c83
CamilleusRex <camilleusrex@gmail.com>
2024-01-30 20:46:20 +0100

    added get_current_user function

--- auth.py
+++ auth.py
@@ -1,5 +1,7 @@
+from fastapi import HTTPException, Depends, status
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
+from auth import oauth2_scheme
 from models import Token, TokenData, User
 
 
@@ -20,3 +22,19 @@ def create_access_token(data: dict, expires_delta: timedelta):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
